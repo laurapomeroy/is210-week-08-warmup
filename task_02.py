cb2ab2f751bf8d753f64de98768e10a0b496a85e
Laura Pomeroy <laura.pomeroy@spsmail.cuny.edu>
2016-10-10 22:27:50 +0000

    Week 8 Warmup Task 2

---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Extend raw_input() usage and convert the data type"""
+
+
+MY_INTEGER = raw_input('Why did the chicken cross the street? ')
+MY_INTEGER = int(MY_INTEGER)
